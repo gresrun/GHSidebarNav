d13f3cae19929d816a5d2807b491ec98b182427b
Greg Haines <gresrun@gmail.com>
2011-11-20 23:21:57 -0500

    Initial Commit

---
+++ GHSidebarNav/GHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GHAppDelegate.h
+//  GHSidebarNav
+//
+//  Created by Greg Haines on 11/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GHSidebarNav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GHSidebarNav
+//
+//  Created by Greg Haines on 11/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHAppDelegate class]));
+	}
+}
